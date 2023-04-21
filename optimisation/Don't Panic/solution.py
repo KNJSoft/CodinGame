b6438b6539c734b18c30aff077d44d2d2ed65178
KNJSoft <knjprod@gmail.com>
2023-04-21 16:11:21 +0100

    Don't Panic

---
+++ optimisation/Don't Panic/solution.py
@@ -0,0 +1,11 @@
+e,v,W,B={},[],"WAIT",'BLOCK'
+o,l,u,z,s,t,a,r=[int(i) for i in input().split()]
+for i in range(r):v.append([int(j) for j in input().split()]);e[v[i][0]]=v[i][1]
+while 1:
+ f,p,d=input().split()
+ f,p=int(f),int(p)
+ if f==z or f==-1:
+  y=(W,B)[(p>s and d[:1]=='R')or(p<s and d[:1]=='L')]
+ else:
+  y=(W,B)[(p>e[f] and d[:1]=='R')or(p<e[f] and d[:1]=='L')]
+ print(y)
